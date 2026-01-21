bdfddf682d53affcc14122b9e3a516d6d70cf2d2
lassehastrup <lasse.hastrup@fellowmind.dk>
2026-01-21 15:53:25 +0100

    Updated UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,54 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { LocationPage } from './pages/LocationPage';
 import { AdminHomePage, AdminLocationPage } from './admin';
 import './App.css';
 
+// Animated background component for public pages
+const AnimatedBackground: React.FC = () => (
+  <>
+    <div className="animated-bg">
+      <div className="orb orb-1"></div>
+      <div className="orb orb-2"></div>
+      <div className="orb orb-3"></div>
+    </div>
+    <div className="particles">
+      {[...Array(10)].map((_, i) => (
+        <div key={i} className="particle"></div>
+      ))}
+    </div>
+    <div className="scanlines"></div>
+  </>
+);
+
+// Layout wrapper that conditionally shows animated background
+const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith('/admin');
+  
+  return (
+    <>
+      {!isAdminRoute && <AnimatedBackground />}
+      {children}
+    </>
+  );
+};
+
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/location/:locationId" element={<LocationPage />} />
-        
-        {/* Admin Routes */}
-        <Route path="/admin" element={<AdminHomePage />} />
-        <Route path="/admin/location/:locationId" element={<AdminLocationPage />} />
-      </Routes>
+      <AppLayout>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/location/:locationId" element={<LocationPage />} />
+          
+          {/* Admin Routes */}
+          <Route path="/admin" element={<AdminHomePage />} />
+          <Route path="/admin/location/:locationId" element={<AdminLocationPage />} />
+        </Routes>
+      </AppLayout>
     </Router>
   );
 };
